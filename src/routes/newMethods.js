a7fbac149ed262475a54de7f70ca5373146cd8cb
thefiend <kam_ming_feng@tech.gov.sg>
2023-06-05 18:49:38 +0800

    Format code

--- src/routes/newMethods.js
+++ src/routes/newMethods.js
@@ -4,18 +4,18 @@ export async function updateTodoById(req, res) {
   const { id } = req.params;
   const updatedTodo = req.body;
   if (id in todoList) {
-      todoList[id] = {...todoList[id], ...updatedTodo};
-      return res.status(200).send();
+    todoList[id] = { ...todoList[id], ...updatedTodo };
+    return res.status(200).send();
   } else {
-      return res.status(400).json({ message: "UUID does not exist" });
+    return res.status(400).json({ message: "UUID does not exist" });
   }
 }
 
 export async function getTodoById(req, res) {
-    const { id } = req.params;
-    if (id in todoList) {
-      return res.status(200).json(todoList[id]);
-    } else {
-      return res.status(400).json({ message: "UUID does not exist" });
-    }
+  const { id } = req.params;
+  if (id in todoList) {
+    return res.status(200).json(todoList[id]);
+  } else {
+    return res.status(400).json({ message: "UUID does not exist" });
+  }
 }
