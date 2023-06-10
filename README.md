b8c840d5715654bf807fd04f66ee55512267f4c9
russell_chua <russell_chua@tech.gov.sg>
2023-06-10 16:13:29 +0800

    add comments to firebase functions and README

--- README.md
+++ README.md
@@ -16,11 +16,15 @@ There are a couple of things you need for this workshop:
   - Docker Desktop for [Windows](https://docs.docker.com/desktop/windows/install/) | [macOS](https://docs.docker.com/desktop/mac/install/)
   - Docker Engine and Docker Compose for [Ubuntu 20.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04) | [Arch Linux](https://wiki.archlinux.org/title/docker)
 
+## Prerequisites: Extra
+
+- Firebase CLI for [Windows](https://firebase.google.com/docs/cli#install-cli-windows) | [macOS](https://firebase.google.com/docs/cli#mac-linux-npm)
+
 ## What's Next?
 
 To learn more about how the application is structured, see [Architecture.md](./ARCHITECTURE.md)
 
-Once you're ready, let's get started with the [Exercise List](https://gds-engineering-bootcamp.gitlab.io/documentation/docs/introduction/GettingStarted)
+Once you're ready, let's get started with the [Exercise List](https://go.gov.sg/learn-dev)
 
 ## Commands
 
--- src/routes/firebase_methods.js
+++ src/routes/firebase_methods.js
@@ -10,50 +10,51 @@ const ERROR_MSGS = {
 Assume Firestore format:
 Collection: todos
 Documents: id
-Fields: {description:string, done:bool}
+Fields: {description:string, done:boolean}
 */
 
+// Get all todo entries
 export async function getAllTodosFirestore(req, res) {
   const toDoRef = db.collection("todos");
-  toDoRef
-    .get()
-    .then(function (data) {
-      const todoList = {};
-      data.forEach((doc) => {
-        todoList[doc.id] = {
-          id: doc.id,
-          description: doc.data().description,
-          done: doc.data().done,
-        };
-      });
-      return res.status(200).json(todoList);
-    })
+
+  toDoRef.get().then(function (data) {
+    const todoList = {};
+    data.forEach((doc) => {
+      todoList[doc.id] = {
+        id: doc.id,
+        description: doc.data().description,
+        done: doc.data().done,
+      };
+    });
+    return res.status(200).json(todoList);
+  })
     .catch((err) => {
       return res.status(500).json({ error: err.code });
     });
 }
 
+// Get todo entry based on todo id
 export async function getTodoByIdFirestore(req, res) {
   const { id } = req.params;
   const document = db.collection("todos").doc(id);
-  document
-    .get()
-    .then((doc) => {
-      if (!doc.exists) {
-        return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
-      }
-      return res.status(200).json({
-        id: doc.id,
-        description: doc.data().description,
-        done: doc.data().done,
-      });
-    })
+
+  document.get().then((doc) => {
+    if (!doc.exists) {
+      return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
+    }
+    return res.status(200).json({
+      id: doc.id,
+      description: doc.data().description,
+      done: doc.data().done,
+    });
+  })
     .catch((err) => {
       console.error(err);
       return res.status(500).json({ error: err.code });
     });
 }
 
+// Post new todo entry
 export async function createTodoFirestore(req, res) {
   const body = req.body;
   if (!("description" in body)) {
@@ -65,32 +66,30 @@ export async function createTodoFirestore(req, res) {
     done: false,
   };
 
-  db.collection("todos")
-    .add(newTodo)
-    .then((doc) => {
-      const responseTodoItem = {
-        id: doc.id,
-        description: newTodo.description,
-        done: newTodo.done,
-      };
-      return res.json(responseTodoItem);
-    })
+  db.collection("todos").add(newTodo).then((doc) => {
+    const responseTodoItem = {
+      id: doc.id,
+      description: newTodo.description,
+      done: newTodo.done,
+    };
+    return res.json(responseTodoItem);
+  })
     .catch((err) => {
       res.status(500).json({ error: err.code });
     });
 }
 
+// Delete todo entry based on todo id
 export async function deleteTodoByIdFirestore(req, res) {
   const { id } = req.params;
   const document = db.collection("todos").doc(id);
-  document
-    .get()
-    .then((doc) => {
-      if (!doc.exists) {
-        return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
-      }
-      document.delete();
-    })
+
+  document.get().then((doc) => {
+    if (!doc.exists) {
+      return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
+    }
+    document.delete();
+  })
     .then(() => {
       res.status(200).json();
     })
@@ -100,19 +99,18 @@ export async function deleteTodoByIdFirestore(req, res) {
     });
 }
 
+// Update todo entry based on todo id
 export async function updateTodoByIdFirestore(req, res) {
   const { id } = req.params;
   const updatedTodo = req.body;
-
   const document = db.collection("todos").doc(id);
-  document
-    .get()
-    .then((doc) => {
-      if (!doc.exists) {
-        return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
-      }
-      document.update(updatedTodo);
-    })
+
+  document.get().then((doc) => {
+    if (!doc.exists) {
+      return res.status(400).json(ERROR_MSGS.NO_SUCH_UUID);
+    }
+    document.update(updatedTodo);
+  })
     .then(() => {
       res.status(200).json();
     })
